e0454356f84e5636d9bebebcd21f0ee59be01378
Luigui Delyer <github@s1x.com.br>
2017-02-18 19:45:22 -0200

    cors

---
+++ src/public/middlewares/cors/index.php
@@ -0,0 +1,13 @@
+<?php
+    /**
+    * MIDDLEWARE to enable CORS
+    */
+    $app->add(function ($req, $res, $next) {
+        $response = $next($req, $res);
+        return $response
+                ->withHeader('Access-Control-Allow-Origin', '*')
+                ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+                ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    });
+
+?>
--- src/public/middlewares/index.php
+++ src/public/middlewares/index.php
@@ -1,5 +1,10 @@
 <?php
     /**
+    * MIDDLEWARE to enable CORS
+    */
+    require_once 'cors/index.php';
+
+    /**
     * MIDDLEWARE to Access-Control-Allow-Methods
     */
     require_once 'allow-methods/index.php';
