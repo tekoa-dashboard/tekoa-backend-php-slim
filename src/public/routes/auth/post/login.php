6a0143dfa849d4ebbb5d6c8b51faaa05b73a6500
Luigui Delyer <github@s1x.com.br>
2018-07-08 21:52:09 +0000

    Sistema de login lindão <3

--- src/public/routes/auth/post/login.php
+++ src/public/routes/auth/post/login.php
@@ -1,108 +1,167 @@
 <?php
-    use \Psr\Http\Message\ServerRequestInterface as Request;
-    use \Psr\Http\Message\ResponseInterface as Response;
+  use \Psr\Http\Message\ServerRequestInterface as Request;
+  use \Psr\Http\Message\ResponseInterface as Response;
 
-    function makeLogin($data) {
-      $user = $data['user'];
-      $password = $data['password'];
+  function validateFields($data) {
+    // Acquire data from form
+    $user = $data['user'];
+    $password = $data['password'];
 
-      if ((!isset($user) ||
-          !isset($password)) ||
-          (empty($user) ||
-          empty($password))) {
-        throw new Exception("You need provide an user and password to login");
+    // If don't have user or password, call exception
+    if ((!isset($user) ||
+        !isset($password)) ||
+        (empty($user) ||
+        empty($password))) {
+      // Call Exception
+      throw new Exception("You need provide an user and password to login");
+      exit;
+    }
+
+    // Encrypt password using key from env to compare on database
+    $password = hash_hmac('sha256', $password, getenv('HASH_CONTENT_KEY'));
+
+    // Return data
+    return array(
+      'user' => $user,
+      'password' => $password
+    );
+  }
+
+  function verifyCredentials($data) {
+    // Acquire data from form
+    $user = $data['user'];
+    $password = $data['password'];
+
+    // Querying from database
+    $query = ORM::for_table(
+        getenv('DB_USERS_TABLE')
+      )
+      ->where(
+        array(
+          getenv('DB_USER_DEFAULT_FIELD') => $user,
+          'password' => $password
+        )
+      )
+      ->find_one();
+
+    if (!$query) {
+      // Call Exception
+      throw new Exception("User or password are incorrect. Is not possible make login now, verify the information provided and try again.");
+      exit;
+    }
+
+    // If query result something, transform in array
+    // else, just return the 'null' result
+    return $query
+      ? $query->as_array()
+      : $query;
+  }
+
+  function makeJWT($data) {
+    // Acquire data from database
+    $id = $data['hash'];
+    $user = $data['user'];
+    $password = $data['password'];
+
+    // Encrypt password again using key from env to write on JWT
+    $password = hash_hmac('sha256', $password, getenv('HASH_CONTENT_KEY'));
+
+    // Config JWT encoder
+    $headers = array(
+      'alg' => 'HS256',
+      'typ' => 'JWT'
+    );
+
+    // Content to serialize
+    $payload = array(
+      'info' => array(
+        'id' => $id,
+        'user' => $user,
+        'pass' => $password
+      ),
+      'sub' => $user,
+      'iat' => time(),
+      'exp' => time() + getenv('JWT_EXPIRATION_TIME')
+    );
+
+    // Secret key
+    $key = getenv('HASH_JWT_KEY');
+
+    // Create JWT
+    $jws = new \Gamegos\JWS\JWS();
+    $hash = $jws->encode($headers, $payload, $key);
+
+    return $hash;
+  }
+
+  /**
+  * POST
+  * AUTH/LOGIN
+  * AUTHENTICATE USER
+  * @route "/auth/login"
+  * @params {string} user THE USER NAME OR ANOTHER FIELD DEFINED ON SETTINGS
+  * @params {string} password USER PASSWORD
+  */
+  $this->map(['POST', 'OPTIONS'], '/login', function (Request $request, Response $response) {
+    try {
+      // Get JWT info from middleware
+      $jwt = $request->getAttribute('jwt');
+
+      // Get request's content
+      $data = $request->getParsedBody();
+
+      // If user's already logged in
+      if ($jwt['logged']) {
+        // Call Exception
+        throw new Exception('You already logged in!');
         exit;
       }
 
-      // Config JWT encoder
-      $headers = array(
-        'alg' => 'HS256',
-        'typ' => 'JWT'
+      // If middleware return some error, call the Exception
+      if (isset($jwt['Error'])) {
+        // Call Exception
+        throw new Exception($jwt['Error']);
+        exit;
+      }
+
+      // If have no data, call the Exception
+      if (empty($data)) {
+        // Call Exception
+        throw new Exception('Data form not found. Provide some data to login.');
+        exit;
+      }
+
+      // Validate fields, Verify credentials on database, and Make JWT
+      $hash = makeJWT(
+        verifyCredentials(
+          validateFields(
+            $data
+          )
+        )
       );
 
-      // Content to serialize
-      $payload = array(
-        'info' => array(
-          'id' => '84b8e4fe93301ed225a0d8bb40c0e90e38818cae4d1e84b4036c5fa926cf9a5b',
-          'user' => $user,
-          'pass' => hash_hmac('sha256', $password, getenv('HASH_PASSWORD_KEY'))
-        ),
-        'sub' => $user,
-        'iat' => time(),
-        'exp' => time() + getenv('JWT_EXPIRATION_TIME')
+      if ($hash) {
+        // Write response object
+        $data = array('Success' => $hash);
+
+        // Create response
+        $response = $response->withJson($data, 201);
+      } else {
+        // Call Exception
+        throw new Exception('Was not possible to make login. Try again.');
+        exit;
+      }
+    } catch (Exception $e) {
+      // Error message
+      $data = array(
+        'Error' => $e->getMessage()
       );
 
-      // Secret key
-      $key = getenv('HASH_JWT_KEY');
-
-      $jws = new \Gamegos\JWS\JWS();
-      $hash = $jws->encode($headers, $payload, $key);
-
-      return $hash;
+      // Create response
+      $response = $response->withJson($data, 400);
     }
 
-    /**
-    * POST
-    * AUTH/LOGIN
-    * AUTHENTICATE USER
-    * @route "/auth/login"
-    * @params {string} user THE USER NAME OR ANOTHER FIELD DEFINED ON SETTINGS
-    * @params {string} password USER PASSWORD
-    */
-    $this->map(['POST', 'OPTIONS'], '/login', function (Request $request, Response $response) {
-        try {
-          // Get JWT info from middleware
-          $jwt = $request->getAttribute('jwt');
-
-          // Get request's content
-          $data = $request->getParsedBody();
-
-          // If user's already logged in
-          if ($jwt['logged']) {
-            // Call Exception
-            throw new Exception('You already logged in!');
-            exit;
-          }
-
-          // If middleware return some error, call the Exception
-          if (isset($jwt['Error'])) {
-            // Call Exception
-            throw new Exception($jwt['Error']);
-            exit;
-          }
-
-          // If have no data, call the Exception
-          if (empty($data)) {
-            // Call Exception
-            throw new Exception('Data form not found. Provide some data to login.');
-            exit;
-          }
-
-          // Make Login
-          $hash = makeLogin($data);
-
-          if ($hash) {
-            // Write response object
-            $data = array('Success' => $hash);
-
-            // Create response
-            $response = $response->withJson($data, 201);
-          } else {
-            // Call Exception
-            throw new Exception('Was not possible to make login. Try again.');
-            exit;
-          }
-        } catch (Exception $e) {
-            // Error message
-            $data = array(
-                'Error' => $e->getMessage()
-            );
-
-            // Create response
-            $response = $response->withJson($data, 400);
-        }
-
-        // send to client
-        return $response;
-    });
+    // send to client
+    return $response;
+  });
 ?>
